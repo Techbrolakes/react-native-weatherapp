4bc2305560686829cf07957dfbc117d110ad837f
Lekan Dar <Lekandar11@gmail.com>
2022-05-29 22:02:10 -1200

    setting up the api

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Button,
+  StatusBar,
+} from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=london&appid=3684c2f87ef57d6a97bf0acdf918a55c";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setValue(response.data);
+        console.log(value);
+      })
+      .catch((error) =>
+        console.log(`This error is present here boss - ${error}`)
+      );
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Working live with react native</Text>
+        <Text> Api Data - {value.name} </Text>
+        <Button title="Click Button" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
